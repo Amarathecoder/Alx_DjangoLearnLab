077ab932859ee1aa9f23e011d6381496439d576e
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-07-20 23:19:32 +0100

    Add role-based access control with UserProfile and views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -32,3 +33,15 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    role_choices = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=role_choices, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,40 +1,23 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
-django.setup()
-
 # Import the models
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
-author_name = "Chinua Achebe"
-try:
+def main():
+    library_name = "National Library"
+    author_name='Chinua Achebe'
     author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
-    print(f"Books by {author_name}:")
+    print("Books by Chinua Achebe:")
     for book in books_by_author:
         print(f"- {book.title}")
-except Author.DoesNotExist:
-    print(f"No author found with name {author_name}")
 
-# 2. List all books in a specific library
-library_name = "National Library"
-try:
     library = Library.objects.get(name=library_name)
     books_in_library = library.books.all()
-    print(f"\nBooks in {library_name}:")
+    print("\nBooks in National Library:")
     for book in books_in_library:
         print(f"- {book.title}")
-except Library.DoesNotExist:
-    print(f"No library found with name {library_name}")
 
-# 3. Retrieve the librarian for a specific library
-try:
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library_name)  # thanks to OneToOne relationship
-    print(f"\nLibrarian managing {library_name}: {librarian.name}")
-except Library.DoesNotExist:
-    print(f"No library found with name {library_name}")
-except Librarian.DoesNotExist:
-    print(f"No librarian assigned to {library_name}")
+    librarian = Librarian.objects.get(library=library)
+    print(f"\nLibrarian for National Library: {librarian.name}")
+
+if __name__ == "__main__":
+    main()
