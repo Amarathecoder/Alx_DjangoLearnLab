d34e84fc324ea140509260292c1625c10e93469a
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-07-20 23:56:31 +0100

    Add custom permissions to Book model and secure views with permission checks

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,38 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-# One Author can write many Books (ForeignKey)
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
-# Each Book has one Author (ForeignKey)
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
-# One Library can have many Books (ManyToMany)
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
-
-# One Librarian manages exactly one Library (OneToOne)
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
 
 class UserProfile(models.Model):
     role_choices = [
@@ -41,7 +33,4 @@ class UserProfile(models.Model):
         ("Member", "Member"),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=role_choices, default='Member')
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+    role = models.CharField(max_length=15, choices=role_choices, default='Member')
