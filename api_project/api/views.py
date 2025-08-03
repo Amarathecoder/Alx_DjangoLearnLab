60e85e165b89af89f72be7766ad3b68ebf470c37
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-08-03 19:47:06 +0100

    Updated view.py with BookViewset and include configuration in urls.py using DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
 from .models import Book
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
