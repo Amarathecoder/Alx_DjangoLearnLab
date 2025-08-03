fe59029f68bd2fbd52bf890a9b4b0f7ec1dd29d5
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-08-03 20:23:23 +0100

    Install & configure authentication and set up permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,16 @@
 from .models import Book
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes =[IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
