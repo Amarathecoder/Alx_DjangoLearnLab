73f4cbde7ed3c29f63b4e24ba66b85c7cd46b954
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-08-17 18:54:56 +0100

    Set up authentication views, templates and configure URL patterns

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,14 @@
+# blog/urls.py
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # Auth Views
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+
+    # Custom Views
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+# blog/views.py
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+     # Handle POST request to update user and profile information
+    if request.method == 'POST':
+        user = request.user
+        
+        # Update user fields like email
+        user.email = request.POST['email']
+        
+        # Update profile fields like bio and profile picture
+        if 'bio' in request.POST:
+            user.profile.bio = request.POST['bio']
+        
+        if 'profile_picture' in request.FILES:
+            user.profile.profile_picture = request.FILES['profile_picture']
+        
+        # Save user and profile
+        user.save()
+        user.profile.save()
+
+        # Redirect to the same profile page after update
+        return redirect('profile')
+
+    # Handle GET request to render profile details
+    return render(request, 'registration/profile.html', {'user': request.user})
